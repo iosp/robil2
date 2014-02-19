cc033515ce6f77bcbc995ebdf2066684079f256a
Dan Erusalimchik <danerde@gmail.com>
2014-02-19 15:00:27 +0200

    RosCom heartbeat method updated.

--- src/Framework/robil2conf/include/ParameterTypesDefs.h
+++ src/Framework/robil2conf/include/ParameterTypesDefs.h
@@ -49,31 +49,31 @@
 typedef std_msgs::String TEMP_TYPE;
 
 namespace types{
-	typedef geometry_msgs::PointStamped Point;
-	typedef geometry_msgs::PoseStamped Pose;
-	typedef geometry_msgs::PoseStamped BladPose; //??? JointState?
-	typedef geometry_msgs::PoseWithCovarianceStamped PoseWithConf;
-	typedef robil_msgs::Path Path;
-	typedef nav_msgs::Odometry Odometry;
-	typedef sensor_msgs::LaserScan Laser;
-	typedef sensor_msgs::Image CamFrame;
-	typedef sensor_msgs::CameraInfo CamInfo;
-	typedef sensor_msgs::Imu Imu;
-	typedef sensor_msgs::NavSatFix GPS;
-	typedef sensor_msgs::NavSatStatus GPSStatus;
-	typedef robil_msgs::Map Map;
-	typedef std_msgs::Float64 WireLength;
-	typedef geometry_msgs::TwistStamped Speed;
-	typedef std_msgs::Int32 Effort;
-	typedef robil_msgs::IEDLocation IEDLocation;
-	typedef std_msgs::String Events;
-	typedef robil_msgs::AssignManipulatorTask ManTask;
-	typedef robil_msgs::AssignNavTask NavTask;
-	typedef robil_msgs::AssignMission Mission;
-	typedef robil_msgs::MissionAcceptance MissionAcceptance;
-	typedef diagnostic_msgs::DiagnosticStatus Diagnostic;
-	typedef robil_msgs::AssignManipulatorTask WSMData;
-	typedef sensor_msgs::JointState Joints;
+	typedef geometry_msgs::PointStamped 				Point;
+	typedef geometry_msgs::PoseStamped 					Pose;
+	typedef geometry_msgs::PoseWithCovarianceStamped 	PoseWithConf;
+	typedef robil_msgs::Path 							Path;
+	typedef nav_msgs::Odometry 							Odometry;
+	typedef sensor_msgs::LaserScan 						Laser;
+	typedef sensor_msgs::Image 							CamFrame;
+	typedef sensor_msgs::CameraInfo 					CamInfo;
+	typedef sensor_msgs::Imu 							Imu;
+	typedef sensor_msgs::NavSatFix 						GPS;
+	typedef sensor_msgs::NavSatStatus 					GPSStatus;
+	typedef robil_msgs::Map 							Map;
+	typedef std_msgs::Float64 							WireLength;
+	typedef geometry_msgs::TwistStamped 				Speed;
+	typedef std_msgs::Int32 							Effort;
+	typedef robil_msgs::IEDLocation 					IEDLocation;
+	typedef std_msgs::String 							Events;
+	typedef robil_msgs::AssignManipulatorTask 			ManTask;
+	typedef robil_msgs::AssignNavTask 					NavTask;
+	typedef robil_msgs::AssignMission 					Mission;
+	typedef robil_msgs::MissionAcceptance 				MissionAcceptance;
+	typedef diagnostic_msgs::DiagnosticStatus 			Diagnostic;
+	typedef robil_msgs::AssignManipulatorTask 			WSMData;
+	typedef sensor_msgs::JointState 					Joints;
+	typedef geometry_msgs::PoseStamped 					BladPose; //??? JointState?
 }
 
 #define COMPONENT context.parameters<Params>().comp
