2b2abfad575f87e2ef477c8ae40c6ece6726e963
Yuval Deri <deriyuval@gmail.com>
2014-03-06 15:49:28 +0200

    adding sensors to live_bobcat, cameras and sick lasers can be visualized, ibeo need more configuration

--- src/Simulation/gazebo_sick_gpu/src/sick.cpp
+++ src/Simulation/gazebo_sick_gpu/src/sick.cpp
@@ -109,11 +109,11 @@ namespace gazebo
 	//populate the LaserScan message
 	sensor_msgs::LaserScan scan;
 	scan.header.stamp = scan_time;
-	scan.header.frame_id = "laser_frame";
+	scan.header.frame_id = this->name_.c_str();
 	scan.angle_min = DegreesToRad(parentSensor->GetAngleMin().Degree());
 	scan.angle_max = DegreesToRad(parentSensor->GetAngleMax().Degree());
 	scan.angle_increment = 3.14 / (double)ranges.size();
-	scan.time_increment = 1000/double(SENSOR_FREQUENCY);
+	scan.time_increment = 1/double(SENSOR_FREQUENCY)/700;
 	scan.range_min = parentSensor->GetRangeMin();
 	scan.range_max = parentSensor->GetRangeMax();
 
