da43f891b2eb070982dd4fa07b6fa159918977c6
Yuval Deri <deriyuval@gmail.com>
2014-03-09 15:53:24 +0200

    adding world tf to bobcat

--- src/Simulation/live_bobcat/src/TFpublisher.cpp
+++ src/Simulation/live_bobcat/src/TFpublisher.cpp
@@ -51,6 +51,15 @@ public:
   gazebo::math::Pose _pose;
 };
 
+
+class dElement
+{
+public:
+  string _child;
+  string _parent;
+  physics::LinkPtr _link;
+};
+
 static tf::TransformBroadcaster br;
 namespace gazebo
 {
@@ -79,11 +88,34 @@ namespace gazebo
 		e._pose.Set(sPose.pos.x,sPose.pos.y,sPose.pos.z,sPose.rot.GetAsEuler().x,sPose.rot.GetAsEuler().y,sPose.rot.GetAsEuler().z);
 
 		i++;
-		_elements.push_back(e);
+		_staticElements.push_back(e);
         }else{
         	break;
         }
+
      }
+//populate dynamic elements
+      i=1;
+      while(true)
+      {
+        stringstream ss;
+        ss << "livetf";
+        ss << i;
+        if (_sdf->HasElement(ss.str()+"frame"))
+        {
+		dElement e;
+                e._child =_sdf->GetElement(ss.str()+"frame")->Get<std::string>();
+		e._parent = "world";
+		e._link = _parent->GetLink(e._child);
+		
+		i++;
+		_dynamicElements.push_back(e);
+        }else{
+        	break;
+        }
+
+     }
+
 
 	double param_update_rate;
 
@@ -113,12 +145,21 @@ namespace gazebo
     		return;
 
         ros::Time time=ros::Time::now();
-     	 for(int i = 0 ; i < _elements.size() ; i++)
+     	 for(int i = 0 ; i < _staticElements.size() ; i++)
 	{
 		tf::Transform transform;
-		transform.setOrigin( tf::Vector3(_elements[i]._pose.pos.x, _elements[i]._pose.pos.y, _elements[i]._pose.pos.z) );
-		transform.setRotation( tf::Quaternion(_elements[i]._pose.rot.x,_elements[i]._pose.rot.y,_elements[i]._pose.rot.z,_elements[i]._pose.rot.w) );
-		br.sendTransform(tf::StampedTransform(transform, time, _elements[i]._parent, _elements[i]._child));
+		transform.setOrigin( tf::Vector3(_staticElements[i]._pose.pos.x, _staticElements[i]._pose.pos.y, _staticElements[i]._pose.pos.z) );
+		transform.setRotation( tf::Quaternion(_staticElements[i]._pose.rot.x,_staticElements[i]._pose.rot.y,_staticElements[i]._pose.rot.z,_staticElements[i]._pose.rot.w) );
+		br.sendTransform(tf::StampedTransform(transform, time, _staticElements[i]._parent, _staticElements[i]._child));
+	}
+
+     	 for(int i = 0 ; i < _dynamicElements.size() ; i++)
+	{
+		gazebo::math::Pose pose=_dynamicElements[i]._link->GetWorldPose();
+		tf::Transform transform;
+			  transform.setOrigin( tf::Vector3(pose.pos.x, pose.pos.y, pose.pos.z) );
+	  transform.setRotation( tf::Quaternion(pose.rot.x,pose.rot.y,pose.rot.z,pose.rot.w) );
+		br.sendTransform(tf::StampedTransform(transform, time, _dynamicElements[i]._parent, _dynamicElements[i]._child));
 	}
 
  	 this->prevUpdateTime = common::Time::GetWallTime();
@@ -134,7 +175,8 @@ namespace gazebo
     physics::ModelPtr 			_model; // Pointer to the model
     event::ConnectionPtr 		_updateConnection; // Pointer to the update event connection
     sensors::CameraSensorPtr 		_sensor;
-    std::vector<sElement> _elements;
+    std::vector<sElement> _staticElements;
+    std::vector<dElement> _dynamicElements;
   };
 
 // Register this plugin with the simulator
