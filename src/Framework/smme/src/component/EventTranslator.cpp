987e80bc6fffbe1cc63e8c9257331302da9312ff
Dan Erusalimchik <danerde@gmail.com>
2014-07-08 18:47:18 +0300

    LLC standby fixed.

--- src/Framework/smme/src/component/EventTranslator.cpp
+++ src/Framework/smme/src/component/EventTranslator.cpp
@@ -14,7 +14,7 @@ void EventTranslator(ComponentMain* comp, decision_making::EventQueue* events)
 	using namespace decision_making;
 #	define TRIGGER Event event=events->waitEvent(); if(not event) return; if(false){}
 #	define ON_EVENT(event_name) else if(event==Event(event_name))
-#	define SEND(event_name) events->raiseEvent(Event(event))
+#	define SEND(event_name) events->raiseEvent(Event(event_name))
 #define X(x) x
 
 	while(true){
