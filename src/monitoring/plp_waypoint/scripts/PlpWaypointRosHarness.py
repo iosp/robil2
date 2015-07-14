dcc9d4d876002f54a27f3259c1fcbfc6ed3ab85f
Michael Bar-Sinai <mich.barsinai@gmail.com>
2015-07-14 18:28:10 +0300

    Added logging info

--- src/monitoring/plp_waypoint/scripts/PlpWaypointRosHarness.py
+++ src/monitoring/plp_waypoint/scripts/PlpWaypointRosHarness.py
@@ -24,7 +24,7 @@ class PlpWaypointRosHarness:
                                  "BOBCAT_AVERAGE_SPEED": 20*1000 # m/hour
                                  })
         self.trigger_state = 0
-        
+
         #Init the ROS stuff
         rospy.init_node("PlpWaypoint", anonymous=False)
         self.publisher = rospy.Publisher(PLP_TOPIC, PlpMessage, queue_size=5)
@@ -32,39 +32,42 @@ class PlpWaypointRosHarness:
         rospy.Subscriber("/PP/Path", Path, self.path_updated)
         rospy.Subscriber("/Loc/Pose", PoseWithCovarianceStamped, self.position_updated)
         rospy.Subscriber(TODO_VALUE, TODO_VALUE, self.nav_task_active)
-    
+        rospy.loginfo("PlpWaypointRosHarness: Started")
+
     def path_updated(self, msg):
+        rospy.loginfo("PlpWaypointRosHarness: Updating Path")
         self.plp.update_path(msg)
-    
+
     def map_updated(self, msg):
+        rospy.loginfo("PlpWaypointRosHarness: Updating Map")
         self.plp.update_map(msg)
-    
+
     def position_updated(self, msg):
+        rospy.loginfo("PlpWaypointRosHarness: Updating Position")
         self.plp.update_position(msg)
-    
+
     def nav_task_active(self, msg):
+        rospy.loginfo("PlpWaypointRosHarness: Task is active!")
         self.advace_trigger_state()
-    
-    def receve_local_path(self, msg):
+
+    def receive_local_path(self, msg):
+        rospy.loginfo("PlpWaypointRosHarness: Local path received")
         self.advace_trigger_state()
-    
+
     def advance_trigger_state(self):
         self.state = self.state + 1
         if ( self.state == 2 ):
             self.attempt_estimation()
             self.state = 0
-        
+
     def attempt_estimation(self):
         """
         Attempts to get estimation from the PLP. May fail if the PLP does not have
         enough information.
         """
+        rospy.loginfo("PlpWaypointRosHarness: Activating PLP")
         if self.plp.can_estimate:
             res = self.plp.get_estimation()
             self.publisher.pubish( PlpMessage(None, "Waypoint", "Estimation", repr(res)))
         else
             self.publisher.publish( PlpMessage(None, "Waypoint", "error", "PLP triggered, but not enough data available"))
-        
-
-        
-    
