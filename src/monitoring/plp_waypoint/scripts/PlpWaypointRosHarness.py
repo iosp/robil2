a108dbd9353f166298bdb12041bdc65a9831b9c8
Michael Bar-Sinai <mich.barsinai@gmail.com>
2015-07-24 17:05:37 +0300

    Messages sent to the PLP topic changed to String

--- src/monitoring/plp_waypoint/scripts/PlpWaypointRosHarness.py
+++ src/monitoring/plp_waypoint/scripts/PlpWaypointRosHarness.py
@@ -73,9 +73,9 @@ class PlpWaypointRosHarness:
         rospy.loginfo("PlpWaypointRosHarness: Activating PLP")
         if self.plp.can_estimate:
             res = self.plp.get_estimation()
-            self.publisher.pubish( PlpMessage(None, "Waypoint", "Estimation", repr(res)))
+            self.publisher.pubish( repr(PlpMessage(None, "Waypoint", "Estimation", repr(res))) ) # TODO remove repr when proper message is used
         else:
-            self.publisher.publish( PlpMessage(None, "Waypoint", "error", "PLP triggered, but not enough data available"))
+            self.publisher.publish( repr(PlpMessage(None, "Waypoint", "error", "PLP triggered, but not enough data available")) ) # TODO remove repr when proper message is used
 
 
 if __name__ == '__main__':
