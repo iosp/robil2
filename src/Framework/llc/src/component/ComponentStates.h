aafdfd6b0eafb40cbc85ee68ab416df5809cbb60
davidx3601 <davidx@post.bgu.ac.il>
2015-10-08 03:07:23 +0300

    Changing the LLC so it will speak with the new Rate2Effort

--- src/Framework/llc/src/component/ComponentStates.h
+++ src/Framework/llc/src/component/ComponentStates.h
@@ -3,4 +3,5 @@
 #include "ComponentMain.h"
 void runComponent(int argc, char** argv, ComponentMain& component);
 void runComponent(int argc, char** argv, ComponentMain* component){runComponent(argc, argv, *component);}
+
 #endif /* COMPONENTSTATES_H_ */
