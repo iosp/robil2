d523be26733d79aba330bc028ba5ff643d946d9c
Ran <rbinyamini@gmail.com>
2016-11-02 18:57:52 +0200

    remove unessasery pkgs from local

--- src/3party/monitoring/scriptable_monitor/include/scriptable_monitor/CustomFunctionsSource.h
+++
@@ -1,8 +0,0 @@
-#define FUNCTION(PlpFunction)\
-	REGISTER_INTERNAL_FUNCTION(PlpFunction)\
-	string PlpFunction::functionName()\
-	{\
-		return #PlpFunction;\
-	}\
-	void PlpFunction::process(Parameters& input, Parameters& output)
-
