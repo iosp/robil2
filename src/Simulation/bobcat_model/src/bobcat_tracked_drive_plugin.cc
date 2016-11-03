aa8170e15e81fc9c426ef1d4f593ac17b9acfec1
Yossi <hawxleader@gmail.com>
2016-11-03 10:34:37 +0200

    minor change of gazebo version number to 2

--- src/Simulation/bobcat_model/src/bobcat_tracked_drive_plugin.cc
+++ src/Simulation/bobcat_model/src/bobcat_tracked_drive_plugin.cc
@@ -1,5 +1,5 @@
 // Written By : Daniel Meltz
-#define MY_GAZEBO_VER 5
+#define MY_GAZEBO_VER 2
 // If the plugin is not defined then define it
 #ifndef _BOBTANK_DRIVE_PLUGIN_HH_
 #define _BOBTANK_DRIVE_PLUGIN_HH_
