5db2999acd9a6add17df97b0ca10bdba3245508a
Daniel Meltz <daniel.meltz@gmail.com>
2016-12-13 15:14:35 +0200

    LOC orientation publication in Roll-Pitch-Yow form for easy testing in rqt
    Signed-off-by: Daniel Meltz <daniel.meltz@gmail.com>

---
+++ src/Framework/robil2rqt/nodes/bupLOC_RolPitchYow.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+import math
+
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import Vector3
+
+
+loc_RPY_pub = rospy.Publisher('/RQT_LOC_RolPithchYow', Vector3 , queue_size=5)
+
+def command_calback(msg):
+		quaternion = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+		euler = tf.transformations.euler_from_quaternion(quaternion)
+		roll = euler[0]
+		pitch = euler[1]
+		yaw = euler[2]
+		
+		RPY = Vector3(roll,pitch,yaw)
+		
+		loc_RPY_pub.publish(RPY)
+
+
+rospy.init_node('rqt_LOC_RolPithchYow_bub')
+
+def main():
+	rate = rospy.Rate(100)
+	while not rospy.is_shutdown():
+		msg = rospy.wait_for_message("/LOC/Pose", PoseWithCovarianceStamped)
+		command_calback(msg)
+		rate.sleep()
+
+main()
+
