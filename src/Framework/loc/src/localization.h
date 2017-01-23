0de4abbc7993d29d53c7d6b8d8dde5261d9493ea
Oded Yechiel <odedyech@post.bgu.ac.il>
2017-01-23 15:44:15 +0200

    Started implementing height estimation

--- src/Framework/loc/src/localization.h
+++ src/Framework/loc/src/localization.h
@@ -9,7 +9,7 @@
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <iostream>
 #include <geometry_msgs/TwistStamped.h>
-#include <tf/transform_broadcaster.h>
+
 #include "component/gps_calculator.h"
 #include <dynamic_reconfigure/server.h>
 #include <dynamic_reconfigure/Reconfigure.h>
@@ -17,6 +17,9 @@
 #include <dynamic_reconfigure/Reconfigure.h>
 #include <dynamic_reconfigure/Config.h>
 #include <dynamic_reconfigure/BoolParameter.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+
 
 using namespace std;
 using namespace sensor_msgs;
@@ -25,36 +28,47 @@ using namespace message_filters;
 
 class localization{
 public:
-localization(ros::NodeHandle& n);
+    localization(ros::NodeHandle& n);
 
-~localization();
-/*
- * This function actually publishes the message and
- * creates a tf tranform between WORLD and ODOM
- * inputs: IMU, GPS, and GPS speed
- * This function calls the publish LOC functions
- */
-void callback(const ImuConstPtr& imu,
-              const NavSatFixConstPtr& gps,
-              const NavSatFixConstPtr& speed_msg);
-/*
- * Publishes the LOC pose and speed messages.
- * Also broadcasts the TF
- **/
-void publishersBroadcasters();
-/*
- * Dynamic reconfiguration callback
- **/
-void configCallback(loc::configConfig &config, uint32_t level);
-/*
- * Set data of speed from messages
- **/
-void handleSpeed(const NavSatFixConstPtr& speed_msg, const ImuConstPtr& imu);
-/*
- * Set data of pose from messages
- **/
-void handlePose(const NavSatFixConstPtr& gps, const ImuConstPtr& imu);
+    ~localization();
+    /*
+     * This function actually publishes the message and
+     * creates a tf tranform between WORLD and ODOM
+     * inputs: IMU, GPS, and GPS speed
+     * This function calls the publish LOC functions
+     */
+    void callback(const ImuConstPtr& imu,
+                  const NavSatFixConstPtr& gps,
+                  const NavSatFixConstPtr& speed_msg);
+    /*
+     * Publishes the LOC pose and speed messages.
+     * Also broadcasts the TF
+     **/
+    void publishersBroadcasters();
+    /*
+     * Dynamic reconfiguration callback
+     **/
+    void configCallback(loc::configConfig &config, uint32_t level);
+    /*
+     * Set data of speed from messages
+     **/
+    void handleSpeed(const NavSatFixConstPtr& speed_msg, const ImuConstPtr& imu);
+    /*
+     * Set data of pose from messages
+     **/
+    void handlePose(const NavSatFixConstPtr& gps, const ImuConstPtr& imu);
+
 private:
+    /*
+     * Transform quaternion message into RPY Vector3 message
+     */
+    geometry_msgs::Vector3 quaternion_to_rpy(geometry_msgs::Quaternion q);
+
+    /*
+     *
+     * Varibales
+     *
+     */
     ros::Publisher loc_pub;
     ros::Publisher speed_pub;
     geometry_msgs::TwistStamped speed;
