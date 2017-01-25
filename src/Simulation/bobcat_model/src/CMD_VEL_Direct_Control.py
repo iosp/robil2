33f9c08081b3cc4045316a4a49fdff3a47533396
yossi2010 <yossicohen2000@gmail.com>
2017-01-25 11:01:19 +0200

    little fixes and added a comfort node for testing.
    Signed-off-by: yossi2010 <yossicohen2000@gmail.com>

---
+++ src/Simulation/bobcat_model/src/CMD_VEL_Direct_Control.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import roslib
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
+publ = rospy.Publisher('/LLC/EFFORTS/Throttle', Float64, queue_size=10)
+pubr = rospy.Publisher('/LLC/EFFORTS/Steering', Float64, queue_size=10)
+v_l=0
+v_r=0
+def callback(msg):
+
+    v_l = msg.linear.x
+    v_r = msg.angular.z
+    rospy.loginfo(v_l)
+    publ.publish(Float64(v_l))
+    pubr.publish(Float64(v_r))
+
+def listener():
+    rospy.init_node('cmd_vel_listener')
+    rospy.Subscriber("/cmd_vel", Twist, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        pass
